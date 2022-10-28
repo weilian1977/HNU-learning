62b60eb76585f74b877479600e30fcda6d0096c4
TDA-2030 <1932489836@qq.com>
2022-10-28 08:24:12 +0800

    update lr

---
+++ logistics_regression/README.md
@@ -0,0 +1,6 @@
+# Mathematics Genealogy Project
+
+<a href="http://www.genealogy.math.ndsu.nodak.edu">
+<img src="http://www.genealogy.math.ndsu.nodak.edu/img/treebutton.gif"
+width="80" height="86" alt="The Mathematics Genealogy Project"
+style="border: 0" /></a>
--- logistics_regression/sketch.py
+++ logistics_regression/sketch.py
@@ -12,31 +12,32 @@ from torch.utils.tensorboard import SummaryWriter
 # torch.utils.tensorboard.SummaryWriter is imported above
 writer = SummaryWriter()
 
-# Download training data from open datasets.
-training_data = datasets.MNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=transforms.ToTensor(),
-)
+def get_data() -> Tuple[np.ndarray, np.ndarray]:
+    # Download training data from open datasets.
+    training_data = datasets.MNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor(),
+    )
 
-test_loader = DataLoader(training_data, batch_size=1, shuffle=False, num_workers=0)
+    test_loader = DataLoader(training_data, batch_size=1, shuffle=False, num_workers=0)
 
-x = np.zeros((28*28, 128))
-y= np.zeros((1, 128))
-w = np.zeros((28*28, 1))
-b = 0
+    m = 28*28*3
+    x = np.zeros((28*28, m))
+    y= np.zeros((1, m))
 
-step=0
-for data in test_loader:
-    imgs, targets = data
-    if targets[0]==1 or targets[0]==2:
-        y[0][step] = targets[0]
-        x[:, step] = imgs.numpy()[0, 0].reshape(784)
-        step+=1
-        if step==128:
-            break
-
+    step=0
+    for data in test_loader:
+        imgs, targets = data
+        if targets[0]==1 or targets[0]==2:
+            y[0][step] = targets[0]
+            x[:, step] = imgs.numpy()[0, 0].reshape(784)
+            step+=1
+            if step==m:
+                break
+    print(f"sample num(m)={step}")
+    return (x, y)
 
 # print(x[:, 127], y)
 
@@ -61,10 +62,16 @@ def backward(X: np.ndarray, A: np.ndarray, Y: np.ndarray) -> Tuple[float, float]
     db = np.sum(db)/m
     return (dw, db)
 
+def test_module():
+    pass
+
+w = np.zeros((28*28, 1))
+b = 0
+x, y = get_data()
 
 for i in range(100):
     A = forward_pop(x, w, b)
     dw, db = backward(x, A, y)
-    print(db)
-    w -= 0.1*dw
-    b -= 0.1*db
+    print(f"i={i}, db={db}, dw200={dw[200]}")
+    w -= 1*dw
+    b -= 1*db
